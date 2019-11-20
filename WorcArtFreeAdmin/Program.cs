01a7b575f07cd289330b196921c3479e893688c9
Goffigus <56737195+Goffigus@users.noreply.github.com>
2019-11-20 09:08:19 -0500

    Add project files.

---
+++ WorcArtFreeAdmin/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static System.Console;
+
+namespace WorcArtFreeAdmin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] univs = {"anna maria college", "eagle hill school",
+                "worcester academy", "worcester polytechnic institute","worcester state university",
+            "amc", "ehs", "wa","wpi","wsu"};
+
+            Array.Sort(univs); // sorts array
+
+            string uCheck = ReadLine();
+
+            int found;
+
+
+
+
+            while (true)
+            {
+                uCheck = uCheck.ToLower(); // sets it all lowercase
+
+                found = Array.BinarySearch(univs, uCheck);
+                
+                if (found >= 0)
+                {
+                    WriteLine("Free Admission");
+
+                }
+                else
+                {
+                    WriteLine("Call security");
+                }
+                uCheck = ReadLine();
+
+            }
+            
+
+        }
+    }
+}
